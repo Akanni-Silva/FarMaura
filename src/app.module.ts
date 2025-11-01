a4af4a69f7cd6381840724fdc39238a0eb5844d8
Akanni Silva <akanni029@gmail.com>
2025-10-31 23:52:51 -0300

    Deploy do Projeto Farmaura

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,21 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Usuario } from './usuario/entities/ususario.entity';
+
 import { UsuarioModule } from './usuario/usuario.module';
 import { PeriodoModule } from './periodo/periodo.module';
-import { Periodo } from './periodo/entities/periodo.entity';
-import { Remedio } from './remedio/entities/remedio.entity';
+
 import { RemediomModule } from './remedio/remedio.module';
 import { AuthModule } from './auth/auth.module';
 import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
+import { ProdService } from './data/services/prod.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'db_farmaura',
-      entities: [Usuario, Periodo, Remedio],
-      synchronize: true,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      useClass: ProdService,
+      imports: [ConfigModule],
     }),
     UsuarioModule,
     PeriodoModule,
---
+++ src/data/services/dev.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+import { Usuario } from '../../usuario/entities/ususario.entity';
+import { Periodo } from '../../periodo/entities/periodo.entity';
+import { Remedio } from '../../remedio/entities/remedio.entity';
+
+@Injectable()
+export class DevService implements TypeOrmOptionsFactory {
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'db_blogpessoal',
+      entities: [Usuario, Periodo, Remedio],
+      synchronize: true,
+    };
+  }
+}
