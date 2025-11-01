ac0f9bac671ea205834ba30ede52b05cc6e0871b
Akanni Silva <akanni029@gmail.com>
2025-11-01 00:08:29 -0300

    Deploy do Projeto Farmaura

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-
 import { UsuarioModule } from './usuario/usuario.module';
 import { PeriodoModule } from './periodo/periodo.module';
 
