0123a7c1cc4bc922bd5acbba9a14eb58ce694246
Akanni <akanni029@gmail.com>
2025-12-02 16:19:22 -0300

    importando usuarioModule para periodos

--- src/periodo/periodo.module.ts
+++ src/periodo/periodo.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Periodo } from './entities/periodo.entity';
 import { PeriodoService } from './services/periodo.service';
 import { PeriodoController } from './controllers/periodo.controller';
+import { UsuarioModule } from '../usuario/usuario.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Periodo])],
+  imports: [TypeOrmModule.forFeature([Periodo]), UsuarioModule],
   providers: [PeriodoService],
   controllers: [PeriodoController],
   exports: [PeriodoService],
